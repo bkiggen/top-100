9747dc3aaf0b96a66798ac5e5b9de9e00419d066
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-01-15 10:40:25 -0600

    Add tests, save new logic before refactor

--- bin/cli.rb
+++ bin/cli.rb
@@ -6,28 +6,50 @@ require 'pry'
 
 # Main Cli class
 class Cli
-  STORE = {}.freeze
+  STORE = {}
 
   def self.run(args)
     new(args).run
   end
 
+  def self.main(args)
+    new(args).main
+  end
+
   def initialize(args)
     @args = args
-    throw_arg_error('none') if @args.count.zero?
-  end
+    throw_arg_error(:none) if @args.count.zero?
 
-  def clean_string(str)
-    str.downcase.gsub(/[^a-z0-9'\s]/i, '')
-  end
-
-  def initialize_text
     @input_docs = []
     @args.each do |doc|
-      throw_arg_error('bad') if !File.file?(doc)
+      throw_arg_error(:bad) if !File.file?(doc)
       new_file = File.readlines(doc, chomp: true)
       @input_docs.concat(new_file)
     end
+    @input_docs = @input_docs.map(&:chomp).join(" ")
+  end
+
+  def main
+    @leftover_words = []
+
+    @input_docs.each do |line|
+      split_line = line.split(' ')
+      process_text(split_line)
+    end
+    STORE
+  end
+
+  def run
+    main
+    format_and_display_output
+  end
+
+  private
+
+   def clean_string(str)
+    return nil unless str.is_a? String
+
+    str.downcase.gsub(/[^a-z0-9'\s]/i, '')
   end
 
   def format_and_display_output
@@ -36,30 +58,35 @@ class Cli
     puts Hirb::Helpers::AutoTable.render(output, { headers: %w[phrase count] })
   end
 
-  def interpret_file(split_line)
-    split_line.each_with_index do |current_word, i|
-      one_ahead = split_line[i + 1]
-      two_ahead = split_line[i + 2]
-
-      if one_ahead && two_ahead
-        data_key = clean_string("#{current_word} #{one_ahead} #{two_ahead}")
-        STORE.key?(data_key) ? STORE[data_key] += 1 : STORE[data_key] = 1
-      else
-        @leftover_words.push(clean_string(current_word)) # fix this
-      end
-    end
+  def update_store(first_word, second_word, third_word)
+    data_key = "#{first_word} #{second_word} #{third_word}"
+    STORE.key?(data_key) ? STORE[data_key] += 1 : STORE[data_key] = 1
   end
 
-  def run
-    initialize_text
-    @leftover_words = []
+  def process_text(split_line)
+    split_line.each_with_index do |word, i|
+      current_word = clean_string(word)
+      one_ahead = clean_string(split_line[i + 1])
+      two_ahead = clean_string(split_line[i + 2])
+      one_behind = clean_string(split_line[i -1])
 
-    @input_docs.each do |line|
-      split_line = line.split(' ')
-      interpret_file(split_line)
+      if i === 0 && @leftover_words.any?
+        update_store(@leftover_words[0], @leftover_words[1], current_word)
+        print "first"
+        print @leftover_words
+        print [@leftover_words[0], @leftover_words[1], current_word]
+        @leftover_words.delete_at(0)
+      elsif i === 1 && @leftover_words.any?
+        update_store(@leftover_words[0], current_word, one_ahead)
+        print "second"
+        print @leftover_words
+        print [@leftover_words[0], one_behind, current_word]
+      elsif one_ahead && two_ahead
+        update_store(current_word, one_ahead, two_ahead)
+      else
+        @leftover_words.push(current_word) 
+      end
     end
-
-    format_and_display_output
   end
 
   def throw_arg_error(type)
--- spec/index_test.rb
+++ spec/index_test.rb
@@ -9,9 +9,41 @@ describe 'Top 100 Phrases CLI' do
       Cli.run([])
     end.to raise_error(ArgumentError)
   end
+
   it 'should throw bad argument error if file does not exist' do
     expect do
       Cli.run(['not_existent'])
     end.to raise_error(ArgumentError)
   end
+
+  it 'should throw return a list of phrase counts if the file exists' do
+    input = Cli.main(['spec/test_texts/very_small.txt'])
+    output = {"i like sandwiches"=>4, "like sandwiches i"=>4, "sandwiches i like"=>2}
+    expect(input).to eq(output)
+  end
 end
+
+# describe 'Methods' do
+#   it 'should throw no argument error with no args provided' do
+#     expect do
+#       Cli.run([])
+#     end.to raise_error(ArgumentError)
+#   end
+
+#   it 'should throw bad argument error if file does not exist' do
+#     expect do
+#       Cli.run(['not_existent'])
+#     end.to raise_error(ArgumentError)
+#   end
+
+#   it 'should throw return a list of phrase counts if the file exists' do
+#     input = Cli.main(['spec/test_texts/very_small.txt'])
+#     output = {"i like sandwiches"=>4, "like sandwiches i"=>4, "sandwiches i like"=>2}
+#     expect(input).to eq(output)
+#   end
+
+#   it 'should throw return a list of phrase counts if the file exists' do
+#     @my_object = MyObject.new
+#     @my_object.send(:do_things_with_message, some_message)
+#   end
+# end
